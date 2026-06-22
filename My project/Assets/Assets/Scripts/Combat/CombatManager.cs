edb2bcf21303d78101ae7eb13ccd2383582ca34c
Srg <srg@CYITLT-DD25FBEF.local>
2026-06-22 11:51:50 +0300

    Revert "Add relic system (data model, acquisition, passive/spin/combat effects, stacking, UI hooks)"
    This reverts commit ae94abfa058c9132569917cb3c2e33961154940a.

--- My project/Assets/Assets/Scripts/Combat/CombatManager.cs
+++ My project/Assets/Assets/Scripts/Combat/CombatManager.cs
@@ -1,12 +1,10 @@
-using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
 public class CombatManager : MonoBehaviour
 {
-    [SerializeField] private int playerHP    = 30;
-    [SerializeField] private int maxPlayerHP = 30;
+    [SerializeField] private int playerHP = 30;
     [SerializeField] private int enemyHP = 20;
     [SerializeField] private int enemyDamage = 5;
     [SerializeField] private bool alreadySpun = false;
@@ -28,15 +26,6 @@ public class CombatManager : MonoBehaviour
 
     private Card.CardColor spunColor;
 
-    public static CombatManager Instance { get; private set; }
-
-    // Events broadcast to the relic system
-    public static event Action<Card.CardColor> OnSpin;
-    public static event Action<int>            OnDamageDealt;
-    public static event Action<int>            OnDamageTaken;
-    public static event Action                 OnEnemyKilled;
-    public static event Action                 OnPlayerTurnStart;
-
     private enum CombatState
     {
         NotStarted,
@@ -48,11 +37,6 @@ public class CombatManager : MonoBehaviour
     }
 
     private CombatState currentState = CombatState.NotStarted;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -74,7 +58,6 @@ public class CombatManager : MonoBehaviour
         currentState = CombatState.PlayerTurn;
         drawCardButton.interactable = false;
         ResetPlayableCards();
-        OnPlayerTurnStart?.Invoke();
     }
 
     void StartEnemyTurn()
@@ -99,7 +82,6 @@ public class CombatManager : MonoBehaviour
 
         spunColor = (Card.CardColor)Random.Range(0, 4);
 
-        OnSpin?.Invoke(spunColor);
         UpdatePlayableCards();
 
     }
@@ -154,11 +136,7 @@ public class CombatManager : MonoBehaviour
             return false;
 
         cardPlayed = true;
-
-        int bonusDamage  = RelicManager.Instance != null ? RelicManager.Instance.GetBonusDamage() : 0;
-        int totalDamage  = card.Data.damage + bonusDamage;
-        DamageEnemy(totalDamage);
-        OnDamageDealt?.Invoke(totalDamage);
+        DamageEnemy(card.Data.damage);
 
         drawCardButton.interactable = true;
 
@@ -196,7 +174,6 @@ public class CombatManager : MonoBehaviour
 
         if (playerHP > 0)
         {
-            OnDamageTaken?.Invoke(damage); // only fire if the player survived the hit
             return false;
         }
 
@@ -207,7 +184,6 @@ public class CombatManager : MonoBehaviour
     private void WinCombat()
     {
         currentState = CombatState.Won;
-        OnEnemyKilled?.Invoke();
         ShowVictory();
     }
 
@@ -243,18 +219,5 @@ public class CombatManager : MonoBehaviour
     void ShowDefeat()
     {
         defeatPanel.SetActive(true);
-    }
-
-    public void HealPlayer(int amount)
-    {
-        playerHP = Mathf.Min(playerHP + amount, maxPlayerHP);
-        UpdateHealthUI();
-    }
-
-    public void GainMaxHP(int amount)
-    {
-        maxPlayerHP += amount;
-        playerHP    += amount;
-        UpdateHealthUI();
     }
 }
